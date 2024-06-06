8aaa2780c8ff4c4053a3a714976c2645c6fd75cd
nadertoti <nadertoti4@gmail.com>
2024-06-06 13:35:06 -0700

    done

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Basic annotations function - to string
+Returns string repr of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Back a string data type
+    """
+    return str(n)
