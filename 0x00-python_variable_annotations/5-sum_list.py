65821658a790a4e8822a81d6ba0349afb8fe0080
nadertoti <nadertoti4@gmail.com>
2024-06-06 13:41:20 -0700

    done

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Complex types - list of floats
+Returns their sum as float
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Return a list of float data typr
+    """
+    return sum(input_list)
