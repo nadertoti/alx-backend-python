0568c8dc3552f12d94c786946ba0f52ea8730fdb
nadertoti <nadertoti4@gmail.com>
2024-06-06 13:32:30 -0700

    done

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Basic annotations function - floor
+Returns floor of the float
+"""
+
+
+def floor(n: float) -> int:
+    """
+    return floor as float data type
+    """
+    return int(n)
