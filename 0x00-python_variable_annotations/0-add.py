45c81c20ce70ceb68c23206c6c0b2af111512dec
nadertoti <nadertoti4@gmail.com>
2024-06-06 13:21:30 -0700

    done

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
+"""
+Basic Annotations - add
+This module contains a function t
+perform addition of two floating-point numbers.
+"""
+
 def add(a: float, b: float) -> float:
+    """
+    Adds two float numbers.
+    """
     return a + b
