3c9fa4c02285261fa5c69c759ad0d8687af42df9
nadertoti <nadertoti4@gmail.com>
2024-06-06 13:29:21 -0700

    done

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Basic Annotations function - concat
+Returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+   concat two string as argument
+    """
+    return str1 + str2
